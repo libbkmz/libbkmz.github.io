feeb9bc47a6c2b8325e96f64a4fc6e481db16a40
Ilya Plenne <libbkmz.dev@gmail.com>
2016-03-01 02:55:13 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,116 @@
+app = angular.module("rare_helper_app", [
+	"ui.bootstrap",
+])
+
+app.controller("main", [
+	"$scope",
+	"systems",
+	"$timeout",
+
+
+	function ($scope, systems, $timeout){
+		$scope.me = {
+			x: 0.0,
+			y: 0.0,
+			z: 0.0,
+		}
+
+		// systems.get_rare_systems($scope.me).then(function (obj){
+		// 	$scope.rare_systems = obj;
+		// });
+
+		$scope.$watch("me", function (oldVal, newVal){
+			// if (oldVal == newVal)
+			// 	return;
+
+			$scope.recalculate_distances();
+		}, true);
+
+		$scope.recalculate_distances = function (){
+			systems.get_rare_systems($scope.me).then(function (obj){
+				$scope.rare_systems = obj;
+			});
+		};
+
+		
+
+
+	}
+]);
+
+app.service("rest_data", [
+	"$http",
+	function ($http){
+		var self = this;
+		self.get_rare_systems = function (){
+			return $http.get("/rare_systems.csv");
+		}
+	}
+]);
+
+app.service("systems", [
+	"rest_data",
+	"$q",
+
+	function (rest_data, $q){
+		var self = this;
+
+		self.get_rare_systems = function (me){
+			// debugger;
+			if (typeof self.rare_systems == 'undefined'){
+				return self._load_rare_systems().then(function (obj){
+					self.rare_systems = obj;
+					self.rare_systems = self.calculate_euclidean_distance(me, self.rare_systems);
+					console.log(obj);
+					return self.rare_systems;
+				})
+			}else {
+				return $q(function (resolve, reject){
+					self.rare_systems = self.calculate_euclidean_distance(me, self.rare_systems);
+					resolve(self.rare_systems);
+				})
+			}
+		};
+		// This function will calcualte distance
+		// distance = sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
+		self.calculate_euclidean_distance = function (me, obj){
+			_x2 = parseFloat(me.x);
+			_y2 = parseFloat(me.y);
+			_z2 = parseFloat(me.z);
+
+			for (var i=0;i<obj.length;i++){
+				_x1 = parseFloat(obj[i].x);
+				_y1 = parseFloat(obj[i].y);
+				_z1 = parseFloat(obj[i].z);
+				obj[i].distance = Math.sqrt(Math.pow(_x1-_x2, 2)+Math.pow(_y1-_y2, 2)+Math.pow(_z1-_z2, 2))
+			}
+			return obj;
+		};
+
+		self._load_rare_systems = function (){
+			return rest_data.get_rare_systems().then(function (response){
+				// parse csv
+				var obj = [];
+				var lines = response.data.split("\n");
+
+				for (var i=0;i<lines.length;i++) {
+					line = lines[i].split(",");
+
+					obj.push({
+						x: line[0],
+						y: line[1],
+						z: line[2],
+						name: line[3],
+					})
+				}
+				return obj;
+			});
+		};
+
+		self.init = function (){
+			
+		};
+
+		self.init();
+	}
+]);
